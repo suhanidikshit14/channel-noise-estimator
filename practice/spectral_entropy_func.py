2cdebc19708e25d6efd4e5f108078ba47694756e
Suhani Dikshit <suhanidikshit14@gmail.com>
2026-06-08 12:38:51 +0530

    modified mistake

--- practice/spectral_entropy_func.py
+++ practice/spectral_entropy_func.py
@@ -10,4 +10,11 @@ X_raw=np.load("data/X_raw.npy")
 y=np.load("data/y_snr.npy")
 entropy=compute_spectral_entropy(X_raw[0])
 print("Spectral Entropy: ",entropy)
-
+#verify Entropy increases as SNR decreases
+snr_values=[20,10,0,-10,-20]
+for snr in snr_values:
+    idx=np.where(y==snr)[0][0]
+    signal=X_raw[idx]
+    entropy=compute_spectral_entropy(signal)
+    print( f"SNR = {snr:>3} dB",
+        f"Entropy = {entropy:.4f}")
